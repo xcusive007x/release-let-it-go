370162146c90478ce0efeb2451c02d14ac186fda
xcusive007x <143580398+xcusive007x@users.noreply.github.com>
2026-05-28 16:32:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Discord webhook (client-side). หากกังวลเรื่อง spam แนะนำย้ายไป server.
-const WEBHOOK_URL = "https://discord.com/api/webhooks/1467760881478926408/A4sjOiHL6yM8UDrLIoB0xiUZe0jLdEO-jjAW2RMPGKff2U_LXM0vTX3M4VAT5tAUN5Zx";
+const WEBHOOK_URL = ENV.WEBHOOK_URL
 
 const MAX_LEN = 4000;
 const DRAFT_KEY = "release.draft.v1";
